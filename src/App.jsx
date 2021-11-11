66bb72b47313c3d5c4b189733cc6ee7651d9ea12
beyond009 <1249413181@qq.com>
2021-11-11 16:10:50 +0800

    -

--- src/App.jsx
+++ src/App.jsx
@@ -16,11 +16,51 @@ import { isDelegationValid } from "@dfinity/authentication";
 import { idlFactory } from "./declarations/backend/backend.did.js";
 import { DelegationIdentity } from "@dfinity/identity";
 import "./App.css";
-
+import test from "./declarations/index.js";
 const App = () => {
   const onDrop = useCallback((acceptedFiles) => {
     // Do something with the files
+    const file = acceptedFiles[0];
+    var fileSize = file.size; // 文件大小
+    var chunkSize = 2 * 1024 * 1024; // 切片的大小
+    var chunks = Math.ceil(fileSize / chunkSize); // 获取切片的个数
+    var blobSlice =
+      File.prototype.slice ||
+      File.prototype.mozSlice ||
+      File.prototype.webkitSlice;
+    var currentChunk = 0;
+    const reader = new FileReader();
+    reader.onload = function (e) {
+      const result = e.target.result;
+      console.log(result);
+      currentChunk++;
+      if (currentChunk < chunks) {
+        loadNext();
+        console.log(
+          `第${currentChunk}分片解析完成，开始解析${currentChunk + 1}分片`
+        );
+      } else {
+        console.log("解析完成");
+      }
+    };
+    const loadNext = async () => {
+      let start = currentChunk * chunkSize;
+      let end = start + chunkSize > fileSize ? fileSize : start + chunkSize;
+      // reader.readAsArrayBuffer(blobSlice.call(file, start, end));
+      console.log(blobSlice.call(file, start, end));
+      currentChunk++;
+      if (currentChunk < chunks) {
+        console.log(
+          `第${currentChunk}分片解析完成，开始解析${currentChunk + 1}分片`
+        );
+        loadNext();
+      } else {
+        console.log("解析完成");
+      }
+    };
+    loadNext();
   }, []);
+
   const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop });
 
   return (
